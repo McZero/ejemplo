15a49bcff2877941f9af8d9175a5fd93afd9f690
Carlos Mtz Muñoz <caemartinezm@gmail.com>
2015-03-03 11:10:20 -0600

    Commit Inicial
    Commit numero uno

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+	<head>
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+	<h2>PHP lenguaje de lado del servidor</h2>
+	<div class="row">
+		<div class="col-md-2"><p>Tabla de multiplicar del 7</p></div>
+		<div class="col-md-4">
+			<select multiple class="form-control">
+			<?php for ($i = 1; $i <= 10; $i++) { ?>
+				<option><?php echo $i*7;?></option>
+			<?php } ?>
+			</select>
+		</div>	
+	</div>
+	<hr>
+	<h2>JAVASCRIPT lenguaje del lado del cliente</h2>
+	<p>Serie de Fibonacci</p>
+	<ol id="fibo">
+		
+	</ol>
+	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script>
+	function seriefibo (n){
+		v1=0;
+		v2=1;
+		$("ol#fibo").append('<li><a class="btn btn-default" href="#" role="button">'+v2+'</a></li>');
+		for (i=1; i<=n; i++)
+		{
+		   Resul = v1 + v2;
+		   v1 = v2;
+		   v2 = Resul;
+		   $("ol#fibo").append('<li><a class="btn btn-default" href="#" role="button">'+v2+'</a></li>');
+		}
+	}
+	seriefibo(10);
+	</script>
+</html>
